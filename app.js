62bb3e389138865d933cd15a80641589422f0bc0
Sheldrick <mrmayrant@yahoo.com>
2018-10-08 04:39:29 -0400

    added the getText and getJson functions to retrieve data from the json and text files

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 document.getElementById("button1").addEventListener("click", getText);
 
+document.getElementById("button2").addEventListener("click", getJson);
+
+// get local text file data
 function getText() {
   fetch("test.txt")
     .then(function(res) {
       return res.text();
     })
     .then(function(data) {
-      console.log(data);
+      document.getElementById("output").innerHTML = `<h5>${data}</h5>`;
     })
     .catch(function(err) {
       console.log(err);
+    });
+}
+
+// get local json file data
+function getJson() {
+  fetch("posts.json")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      let output = "";
+      data.forEach(post => {
+        output += `<li>${post.title}</li>`;
+      });
+      document.getElementById("output").innerHTML = output;
     });
 }
