49377814ff08ec8f6b0ccf995cd2579938b04997
Sheldrick <mrmayrant@yahoo.com>
2018-10-08 05:23:20 -0400

    refactored the code to utilize arrow functions

--- app.js
+++ app.js
@@ -7,43 +7,33 @@ document.getElementById("button3").addEventListener("click", getExternal);
 // get local text file data
 function getText() {
   fetch("test.txt")
-    .then(function(res) {
-      return res.text();
-    })
-    .then(function(data) {
+    .then(res => res.text())
+    .then(data => {
       document.getElementById("output").innerHTML = `<h5>${data}</h5>`;
     })
-    .catch(function(err) {
-      console.log(err);
-    });
+    .catch(err => console.log(err));
 }
 
 // get local json file data
 function getJson() {
   fetch("posts.json")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
+    .then(response => response.json())
+    .then(data => {
       let output = "";
-      data.forEach(post => {
-        output += `<li>${post.title}</li>`;
-      });
+      data.forEach(post => (output += `<li>${post.title}</li>`));
       document.getElementById("output").innerHTML = output;
-    });
+    })
+    .catch(err => console.log(err));
 }
 
 // get external api data
 function getExternal() {
   fetch("https://api.github.com/users")
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
+    .then(response => response.json())
+    .then(data => {
       let output = "";
-      data.forEach(user => {
-        output += `<li>Github User: ${user.login}</li>`;
-      });
+      data.forEach(user => (output += `<li>Github User: ${user.login}</li>`));
       document.getElementById("output").innerHTML = output;
-    });
+    })
+    .catch(err => console.log(err));
 }
